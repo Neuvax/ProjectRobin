93080c26def5e849549e24601ec494245007deb0
Ingrid González <glez.ingrid23@gmail.com>
2023-06-11 20:34:12 -0600

    cpu mod

--- ProjectRobin/CPU/cpu.py
+++ ProjectRobin/CPU/cpu.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 
 robot_x = 0
 robot_y = 0
@@ -74,9 +75,35 @@ def execute_actions_from_file(file_path):
             except ValueError:
                 print("Invalid Operation, not enough values")
 
+def compile_yacc(file):
+    yacc_command = f"yacc -d {file}"
+    subprocess.run(yacc_command, shell=True)
+
+def compile_lex(file):
+    lex_command = f"lex {file}"
+    subprocess.run(lex_command, shell=True)
+
+def compile_and_run(yacc_file, lex_file, input_file):
+    compile_yacc(yacc_file)
+    compile_lex(lex_file)
+    subprocess.run("cc lex.yy.c y.tab.c -o ejecutable.exe", shell=True)
+    subprocess.run(f"./ejecutable.exe {input_file}", shell=True)
+
+def read_asm_file(file):
+    with open(file, 'r') as asm_file:
+        asm_code = asm_file.read()
+    return asm_code
+
 def main():
+    yacc_file = "../Compiler/compiler.y"
+    lex_file = "../Compiler/compiler.l"
+    input_file = "../Compiler/input.txt"
+
+    compile_and_run(yacc_file, lex_file, input_file)
+    read_asm_file('output.asm')
     plot()
-    execute_actions_from_file('ProjectRobin\Deliverable 01\instructions.asm')
+    execute_actions_from_file('output.asm')
+
 
 if __name__ == "__main__":
     main()
